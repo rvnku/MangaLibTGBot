605a9b769e692cb1c4006f838c6d57b33ee2e96a
Rinny <artpr.zxc@gmail.com>
2024-12-30 11:03:23 +0500

    domens are updated

--- src/config/actual_domen_names.py
+++ src/config/actual_domen_names.py
@@ -1,21 +1,17 @@
 ACTUAL_DOMEN_NAMES = {
     1: [
         "mangalib.me",
-        "mangalib.org",
-        "test-front.mangalib.me"
+        "mangalib.org"
     ],
     2: [
-        "v1.yaoilib.net",
-        "test-front.slashlib.me"
+        "v1.yaoilib.net"
     ],
     3: [
-        "ranobelib.me",
-        "test-front.ranobelib.me"
+        "ranobelib.me"
     ],
     4: [
         "hentailib.me",
-        "v1.hentailib.org",
-        "test-front.hentailib.me"
+        "v1.hentailib.org"
     ],
     5: [
         "anilib.me"
